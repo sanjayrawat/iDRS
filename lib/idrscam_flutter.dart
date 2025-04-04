4bd5a6948535523ce712416609bddddf568df8e1
sanjay rawat <sanjayrawat452@gmail.com>
2025-04-04 17:22:26 +0530

    cm
    dd

---
+++ lib/idrscam_flutter.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatelessWidget {
+  const CameraScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Camera Screen")),
+      body: const Center(child: Text("Camera functionality coming soon")),
+    );
+  }
+}
---
+++ lib/idrscam_history_screen.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class ReplayHistoryScreen extends StatelessWidget {
+  const ReplayHistoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Replay History")),
+      body: const Center(child: Text("Replay history will appear here")),
+    );
+  }
+}
